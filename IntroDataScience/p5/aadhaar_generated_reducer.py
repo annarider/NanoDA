7fa2acbd830e43f8eef80f7c8052529debdab34e
Anna Li <annax.li10@gmail.com>
2015-01-01 19:52:07 -0800

    added instructor generated code. haven't started yet

---
+++ IntroDataScience/p5/aadhaar_generated_reducer.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
+
+def reducer():
+    
+    #Also make sure to fill out the mapper code before clicking "Test Run" or "Submit".
+
+    #Each line will be a key-value pair separated by a tab character.
+    #Print out each key once, along with the total number of Aadhaar 
+    #generated, separated by a tab. Make sure each key-value pair is 
+    #formatted correctly! Here's a sample final key-value pair: 'Gujarat\t5.0'
+
+    #Since you are printing the output of your program, printing a debug 
+    #statement will interfere with the operation of the grader. Instead, 
+    #use the logging module, which we've configured to log to a file printed 
+    #when you click "Test Run". For example:
+    #logging.info("My debugging message")
+        
+    for line in sys.stdin:
+        # your code here
+
+reducer()
