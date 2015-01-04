f018499125f74afe0763b405c1ad5f801749ff15
Anna Li <annax.li10@gmail.com>
2015-01-04 03:51:38 -0800

    finished aadhaar generated reducer & submitted successfully to grader

--- IntroDataScience/p5/aadhaar_generated_reducer.py
+++ IntroDataScience/p5/aadhaar_generated_reducer.py
@@ -37,23 +37,4 @@ def reducer():
         print "{0}\t{1}".format(key, aadhaar_generated[key])
     
 
-    old_key = None
-    key_history = []
-
-    for line in sys.stdin:
-        # data = line.strip.split("\t")
-
-        if len(data) == 2:
-            this_key, aadhaar_count = data
-
-            if old_key and old_key != this_key:
-                print "{0}\t{1}".format(old_key, aadhaar_count)
-                aadhaar_count = 0
-
-            old_key = this_key
-            aadhaar_count += 1
-
-        if old_key != None:
-            print "{0}\t{1}".format(old_key, aadhaar_count)
-
 reducer()
