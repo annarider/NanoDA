a72929775360ff064dbd064fab8bc8a1856d0c02
Anna Li <annax.li10@gmail.com>
2015-01-04 20:31:24 -0800

    fixed stats test & writing up findings

--- IntroDataScience/Project/SubwayDataShapiroWilk.py
+++ IntroDataScience/Project/SubwayDataShapiroWilk.py
@@ -8,12 +8,9 @@ def calc_shapwilk_subway(subway_df):
     without_rain = subway_df['ENTRIESn_hourly'][subway_df['rain'] == 0]
 
     # reference: http://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.shapiro.html
-    shapwilk_test = scipy.stats.shapiro(with_rain)
+    shapwilk_test = [scipy.stats.shapiro(with_rain), scipy.stats.shapiro(without_rain)]
 
-    if shapwilk_test[1] < .05:
-        return False, shapwilk_test
-    else:
-        return True, shapwilk_test
+    return shapwilk_test
 
 
 if __name__ == '__main__':
