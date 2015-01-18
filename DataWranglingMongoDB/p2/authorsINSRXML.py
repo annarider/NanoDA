0fdb485b5778639582ddf9f82a986eab67bdae7b
Anna Li <annax.li10@gmail.com>
2015-01-17 19:51:31 -0800

    begin XML parsing exercises

---
+++ DataWranglingMongoDB/p2/authorsINSRXML.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# Your task here is to extract data from xml on authors of an article
+# and add it to a list, one item for an author.
+# See the provided data structure for the expected format.
+# The tags for first name, surname and email should map directly
+# to the dictionary keys, but you have to extract the attributes from the "insr" tag
+# and add them to the list for the dictionary key "insr"
+import xml.etree.ElementTree as ET
+
+article_file = "exampleResearchArticle.xml"
+
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def get_authors(root):
+    authors = []
+    insr_iid = []
+    for author in root.findall('./fm/bibl/aug/au'):
+
+        # get author data - firname, surname, email
+        firstname = author.find('fnm')
+        surname = author.find('snm')
+        email = author.find('email')
+        
+        # iterate through author data to retrieve all insr info
+        for iid in author.iter('insr'):
+            insr_iid.append(iid.get('iid'))
+
+
+        data = {
+                "fnm": firstname.text,
+                "snm": surname.text,
+                "email": email.text,
+                "insr": insr_iid
+        }
+
+        # YOUR CODE HERE
+
+        authors.append(data)
+
+    return authors
+
+
+def test():
+    solution = [{'insr': ['I1'], 'fnm': 'Omer', 'snm': 'Mei-Dan', 'email': 'omer@extremegate.com'},
+                {'insr': ['I2'], 'fnm': 'Mike', 'snm': 'Carmont', 'email': 'mcarmont@hotmail.com'},
+                {'insr': ['I3', 'I4'], 'fnm': 'Lior', 'snm': 'Laver', 'email': 'laver17@gmail.com'},
+                {'insr': ['I3'], 'fnm': 'Meir', 'snm': 'Nyska', 'email': 'nyska@internet-zahav.net'},
+                {'insr': ['I8'], 'fnm': 'Hagay', 'snm': 'Kammar', 'email': 'kammarh@gmail.com'},
+                {'insr': ['I3', 'I5'], 'fnm': 'Gideon', 'snm': 'Mann', 'email': 'gideon.mann.md@gmail.com'},
+                {'insr': ['I6'], 'fnm': 'Barnaby', 'snm': 'Clarck', 'email': 'barns.nz@gmail.com'},
+                {'insr': ['I7'], 'fnm': 'Eugene', 'snm': 'Kots', 'email': 'eukots@gmail.com'}]
+
+    root = get_root(article_file)
+    data = get_authors(root)
+
+    print data[0]
+    print solution[0]
+
+    assert data[0] == solution[0]
+
+    assert data[1]["insr"] == solution[1]["insr"]
+
+
+test()
---
+++ DataWranglingMongoDB/p2/authorsNameEmailXML-SOLUTION.py
@@ -0,0 +1,42 @@
+'''
+Solution!!!
+'''
+import xml.etree.ElementTree as ET
+
+article_file = "exampleResearchArticle.xml"
+
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def get_author(root):
+    authors = []
+    for author in root.findall('./fm/bibl/aug/au'):
+        data = {
+                "fnm": None,
+                "snm": None,
+                "email": None
+        }
+        data["fnm"] = author.find('./fnm').text
+        data["snm"] = author.find('./snm').text
+        data["email"] = author.find('./email').text
+
+        authors.append(data)
+
+    return authors
+
+
+def test():
+    solution = [{'fnm': 'Omer', 'snm': 'Mei-Dan', 'email': 'omer@extremegate.com'}, {'fnm': 'Mike', 'snm': 'Carmont', 'email': 'mcarmont@hotmail.com'}, {'fnm': 'Lior', 'snm': 'Laver', 'email': 'laver17@gmail.com'}, {'fnm': 'Meir', 'snm': 'Nyska', 'email': 'nyska@internet-zahav.net'}, {'fnm': 'Hagay', 'snm': 'Kammar', 'email': 'kammarh@gmail.com'}, {'fnm': 'Gideon', 'snm': 'Mann', 'email': 'gideon.mann.md@gmail.com'}, {'fnm': 'Barnaby', 'snm': 'Clarck', 'email': 'barns.nz@gmail.com'}, {'fnm': 'Eugene', 'snm': 'Kots', 'email': 'eukots@gmail.com'}]
+    
+    root = get_root(article_file)
+    data = get_authors(root)
+
+    assert data[0] == solution[0]
+    assert data[1]["fnm"] == solution[1]["fnm"]
+
+
+if __name__ == '__main__':
+    test()
