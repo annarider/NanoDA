bcb45ff503bd0c97c2e17a37d45a648fde26d69b
Anna Li <annax.li10@gmail.com>
2015-01-22 16:03:55 -0800

    got working code that tests by row and now column, grrr'

--- DataWranglingMongoDB/p3/audit-cities.py
+++ DataWranglingMongoDB/p3/audit-cities.py
@@ -14,7 +14,7 @@ The possible types of values can be:
 The audit_file function should return a dictionary containing fieldnames and a set of the datatypes
 that can be found in the field.
 All the data initially is a string, so you have to do some checks on the values first.
-
+ou 
 """
 import codecs
 import csv
@@ -28,12 +28,50 @@ FIELDS = ["name", "timeZone_label", "utcOffset", "homepage", "governmentType_lab
           "areaLand", "areaMetro", "areaUrban"]
 
 def audit_file(filename, fields):
-    fieldtypes = {}
+    fieldtypes = { 'NoneType': 0, 'list': 0, 'int': 0, 'float': 0, 'str':0 }
 
-    # YOUR CODE HERE
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
 
+        # iterate through every row
+        for row in reader:
+            # select specific columns to test the data type
+            for field in FIELDS:
+                # start testing the data types
+                # if field has value 'NULL' or empty string, add to NoneType
+                if row[field] == 'NULL' or row[field] == '':
+                    fieldtypes['NoneType'] += 1
+                # if field has value that starts with '{', add to list
+                if '{' in row[field]:
+                    fieldtypes['list'] += 1
+                if check_valid_int(row[field]):
+                    fieldtypes['int']
+                if not check_valid_int and check_valid_float:
+                    fieldtypes['float']
+                else:
+                    fieldtypes['str'] += 1
 
     return fieldtypes
+
+'''
+Check if the number is a valid int
+'''
+def check_valid_int(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+'''
+Check if the number is a valid float
+'''
+def check_valid_float(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 def test():
