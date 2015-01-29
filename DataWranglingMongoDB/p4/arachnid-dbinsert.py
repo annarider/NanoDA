36d3a3ae21e4de7c4996ff2e823739f3831d7a8e
Anna Li <annax.li10@gmail.com>
2015-01-28 23:04:17 -0800

    starting exercise 2 from problem set 4

---
+++ DataWranglingMongoDB/p4/arachnid-dbinsert.py
@@ -0,0 +1,18 @@
+import json
+
+def insert_data(data, db):
+
+    for e in data:
+        db.data.insert(a)
+
+
+if __name__ == "__main__":
+    
+    from pymongo import MongoClient
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.examples
+
+    with open('arachnid.json') as f:
+        data = json.loads(f.read())
+        insert_data(data, db)
+        print db.arachnid.find_one()
