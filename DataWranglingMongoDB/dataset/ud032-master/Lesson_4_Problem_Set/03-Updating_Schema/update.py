78001575ebbfc7eb76b4454eafb66eb5559e10e5
Anna Li <annax.li10@gmail.com>
2015-02-05 11:35:15 -0800

    starting to understand what's needed for arachnid update

--- DataWranglingMongoDB/dataset/ud032-master/Lesson_4_Problem_Set/03-Updating_Schema/update.py
+++ DataWranglingMongoDB/dataset/ud032-master/Lesson_4_Problem_Set/03-Updating_Schema/update.py
@@ -1,48 +1,44 @@
-#!/usr/bin/env python # -*- coding: utf-8 -*- 
-
-""" In this problem set you work
-with another type of infobox data, audit it, clean it,  come up with a data
-model, insert it into a MongoDB and then run some queries against your database.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+In this problem set you work with another type of infobox data, audit it, clean it, 
+come up with a data model, insert it into a MongoDB and then run some queries against your database.
 The set contains data about Arachnid class.
 
-The data is already in the database. But you have been given a task to also
-include 'binomialAuthority' information in the data, so you have to go through
-the data and update the existing entries.
+The data is already in the database. But you have been given a task to also include 'binomialAuthority'
+information in the data, so you have to go through the data and update the existing entries.
 
-The following things should be done in the function add_field: 
+The following things should be done in the function add_field:
 - process the csv file and extract 2 fields - 'rdf-schema#label' and 'binomialAuthority_label'
 - clean up the 'rdf-schema#label' same way as in the first exercise - removing redundant "(spider)" suffixes
 - return a dictionary, with 'label' being the key, and 'binomialAuthority_label' the value
 - if 'binomialAuthority_label' is "NULL", skip the item
 
-
-The following should be done in the function update_db: 
+The following should be done in the function update_db:
 - query the database by using the field 'label'
 - update the data, by adding a new item under 'classification' with a key 'binomialAuthority'
 
-The resulting data should look like this: 
-- the output structure should be as follows: 
 
-{   'label': 'Argiope',   
-    'uri': 'http://dbpedia.org/resource/Argiope_(spider)',   
-    'description': 'The genus Argiope includes rather large and spectacular spiders that often ...',   
-    'name': 'Argiope',   
-    'synonym': ["One", "Two"],   
-    'classification': {'binomialAuthority': None, 
-                       'family': 'Orb-weaver spider',
-                       'class': 'Arachnid',                     
-                       'phylum': 'Arthropod',
-                       'order': 'Spider',                    
-                       'kingdom': 'Animal',
-                       'genus': None                 
-                       } 
+The resulting data should look like this:
+- the output structure should be as follows:
+{ 'label': 'Argiope',
+  'uri': 'http://dbpedia.org/resource/Argiope_(spider)',
+  'description': 'The genus Argiope includes rather large and spectacular spiders that often ...',
+  'name': 'Argiope',
+  'synonym': ["One", "Two"],
+  'classification': {
+                    'binomialAuthority': None,
+                    'family': 'Orb-weaver spider',
+                    'class': 'Arachnid',
+                    'phylum': 'Arthropod',
+                    'order': 'Spider',
+                    'kingdom': 'Animal',
+                    'genus': None
+                    }
 }
-
-
-""" 
-
-import codecs 
-import csv 
+"""
+import codecs
+import csv
 import json
 import pprint
 
@@ -83,8 +79,7 @@ def test():
     update_db(data, db)
 
     updated = db.arachnid.find_one({'label': 'Opisthoncana'})
-    print updated
-    # assert updated['classification']['binomialAuthority'] == 'Embrik Strand'
+    assert updated['classification']['binomialAuthority'] == 'Embrik Strand'
     pprint.pprint(data)
 
 
