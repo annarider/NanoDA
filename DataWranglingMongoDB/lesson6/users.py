fbadb94e20933fed0f740ceb0832e72fdf62b84e
Anna Li <annax.li10@gmail.com>
2015-02-26 21:07:53 -0800

    completed the users query

--- DataWranglingMongoDB/lesson6/users.py
+++ DataWranglingMongoDB/lesson6/users.py
@@ -12,20 +12,30 @@ The function process_map should return a set of unique user IDs ("uid")
 """
 
 def get_user(element):
-    return
-
+    # retrieve the dictionary with all items in the element
+    element_dict = element.attrib
+    # only proceed if 'user' is a valid attribute in the element
+    if 'user' in element_dict:
+        # find the value of 'user' in the node element
+        user = element_dict['user']
+        return user
+    else:
+        # didn't find 'user' so return None
+        return None
 
 def process_map(filename):
     users = set()
     for _, element in ET.iterparse(filename):
-        
-
+        user = get_user(element)
+        # if valid user, add to set (set will only add unique users)
+        if user != None:
+            users.add(user)
     return users
 
 
 def test():
 
-    users = process_map('example.osm')
+    users = process_map('example2.osm')
     pprint.pprint(users)
     assert len(users) == 6
 
