a80d01748d76fb75bd80afe2fe93d01c6ae8b513
Anna Li <annax.li10@gmail.com>
2015-03-01 00:03:42 -0800

    auditing addresses in mongodb

--- DataWranglingMongoDB/Project2/akladdress.py
+++ DataWranglingMongoDB/Project2/akladdress.py
@@ -1,3 +1,5 @@
+import pprint
+from pymongo import MongoClient
 '''
 Intro: This script queries the MongoDB db auckland, collection full to retrieve
 the osm data on Auckland after it has been preliminarily cleaned, transformed
@@ -8,13 +10,20 @@ to ensure consistent data values.
 '''
 
 def get_db(db_name):
-    from pymongo import MongoClient
+    
     client = MongoClient('localhost:27017')
     db = client[db_name]
     return db
 
-def make_pipeline():
+def retrieve_streets():
     pipeline = [ {'$group': {'_id': '$address.street',
+                               'count': { '$sum': 1} }},
+                 {'$sort': {'count': -1}},
+                 {'$limit': 100} ]
+    return pipeline
+
+def retrieve_cities():
+    pipeline = [ {'$group': {'_id': '$address.city',
                                'count': { '$sum': 1} }},
                  {'$sort': {'count': -1}},
                  {'$limit': 100} ]
@@ -28,8 +37,11 @@ def aggregate(db, pipeline):
 if __name__ == '__main__':
     # access the db auckland with osm data on auckland
     db = get_db('auckland')
-    pipeline = make_pipeline()
-    result = aggregate(db, pipeline)
-    import pprint
-    pprint.pprint(result)
+    # pipeline_street = retrieve_streets()    
+    # result_street = aggregate(db, pipeline_street)
+    # pprint.pprint(result_street)
+
+    pipeline_city = retrieve_cities()
+    result_city = aggregate(db, pipeline_city)
+    pprint.pprint(result_city)
     
