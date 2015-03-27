947b7e5423471b151d0219136442fd3b1702dff6
Anna Li <annax.li10@gmail.com>
2015-03-26 18:30:52 -0700

    refine plots

--- EDAwithR/EDA_Course_Materials/lesson4/lesson4_problem_set/price v carat.R
+++ EDAwithR/EDA_Course_Materials/lesson4/lesson4_problem_set/price v carat.R
@@ -5,11 +5,11 @@
 
 ggplot(data = diamonds, aes(x = carat, y = price)) +
     geom_point() +
-    scale_x_continuous(quantile(diamonds$carat, 0.9)) +
-    scale_y_continuous(quantile(diamonds$price, 0.9)) +
+    scale_x_continuous(limits = c(0, quantile(diamonds$carat, 0.99))) +
+    scale_y_continuous(limits = c(0, quantile(diamonds$price, 0.99))) +
     xlab("carat") +
     ylab("price")
 
 
-    xlim(0, quantile(diamonds$carat, 0.9)) +
-    ylim(0, quantile(diamonds$price, 0.9))
+quantile(diamonds$carat, .99)
+
--- EDAwithR/EDA_Course_Materials/lesson4/lesson4_problem_set/price v volume.R
+++ EDAwithR/EDA_Course_Materials/lesson4/lesson4_problem_set/price v volume.R
@@ -26,3 +26,18 @@ detach("package:plyr", unload=TRUE)
 # in later exercises.
 
 cor.test(diamonds$price, diamonds$volume)
+diamonds_volume <- subset(diamonds, volume > 0 & volume <= 800)
+cor.test(diamonds_volume$price, diamonds_volume$volume)
+
+# Subset the data to exclude diamonds with a volume
+# greater than or equal to 800. Also, exclude diamonds
+# with a volume of 0. Adjust the transparency of the
+# points and add a linear model to the plot. (See the
+# Instructor Notes or look up the documentation of
+# geom_smooth() for more details about smoothers.)
+
+# We encourage you to think about this next question and
+# to post your thoughts in the discussion section.
+
+# Do you think this would be a useful model to estimate
+# the price of diamonds? Why or why not?
