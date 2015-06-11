0e0173c0a37684aac7638142297a8077eff349f9
Anna Li <annax.li10@gmail.com>
2015-06-11 00:15:16 -0700

    svm mini-project, slicing data to 1%

--- IntroMachineLearning/svm/svm_author_id.py
+++ IntroMachineLearning/svm/svm_author_id.py
@@ -32,16 +32,16 @@ labels_train = labels_train[:len(labels_train)/100]
 ### timing to train algorithm
 t0 = time()
 clf.fit(features_train, labels_train)
-### print time taken to  train -- 199.766 s
+### print time taken to  train -- 199.766 s / 0.114 s
 print "training time:", round(time()-t0, 3), "s"
 
 ### timing to test algorithm
 t1 = time()
 pred = clf.predict(features_test)
-### print time taken to test -- 19.506 s
+### print time taken to test -- 19.506 s / 1.368 s
 print "testing time:", round(time()-t1, 3), "s"
 
-# predict accuracy -- 0.984072810011
+# predict accuracy -- 0.984072810011 / 0.884527872582
 acc = accuracy_score(pred, labels_test)
 print acc
 
