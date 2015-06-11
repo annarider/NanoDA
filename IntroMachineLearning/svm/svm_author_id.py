cc3d00d1a6dd651797409a4f371e60d5b4980c8f
Anna Li <annax.li10@gmail.com>
2015-06-11 00:12:38 -0700

    svm mini-project

--- IntroMachineLearning/svm/svm_author_id.py
+++ IntroMachineLearning/svm/svm_author_id.py
@@ -14,19 +14,35 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
 
-#########################################################
+clf = SVC(kernel="linear")
+
+# reduce size of training set to speed up svm 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+### timing to train algorithm
+t0 = time()
+clf.fit(features_train, labels_train)
+### print time taken to  train -- 199.766 s
+print "training time:", round(time()-t0, 3), "s"
+
+### timing to test algorithm
+t1 = time()
+pred = clf.predict(features_test)
+### print time taken to test -- 19.506 s
+print "testing time:", round(time()-t1, 3), "s"
+
+# predict accuracy -- 0.984072810011
+acc = accuracy_score(pred, labels_test)
+print acc
 
 
