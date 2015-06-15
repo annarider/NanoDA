2bfc3a101021690a048b52ea648562b7fb041e55
Anna Li <annax.li10@gmail.com>
2015-06-14 20:28:41 -0700

    exploring Enron dataset

---
+++ IntroMachineLearning/datasets_questions/explore_num_people.py
@@ -0,0 +1,55 @@
+from explore_enron_data import enron_data
+import os
+
+def audit_num_people():
+
+    # print enron_data
+
+    print "number of people/data points:", len(enron_data)
+
+    print "number of features available:", len(enron_data.itervalues().next())
+
+    # Calculate number of people who are points of interest
+    num_poi = 0
+    for key in enron_data: 
+        if 'poi' in enron_data[key]:
+            if enron_data[key]['poi'] == True:
+                num_poi += 1    
+    print "number of poi:", num_poi 
+
+def count_names(filename):
+    os.chdir('../final_project')
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    num_ppl = 0
+    for row in lines: 
+        if '(' in row:
+            num_ppl += 1 
+    print "number of people:", num_ppl
+
+def audit_james_prentice(feature):    
+
+    # find value of stock belonging to James Prentice
+    for key in enron_data:
+        if key == 'PRENTICE JAMES': 
+            print "Jame Prentice's total value of stock:", enron_data[key][feature]
+
+def audit_wesley_colwell(feature):
+    for key in enron_data:
+        if key == 'COLWELL WESLEY': 
+            print "email messages from Wesley Colwell to poi:", enron_data[key][feature]
+
+def see_features():
+    # find all possible features for individuals
+    for key in enron_data.keys():
+        # print "key: %s, value: %s" % (key, enron_data[key])
+        print enron_data[key].keys()
+        break
+
+if __name__ == '__main__':
+    # audit_num_people()
+    # count_names('poi_names.txt')
+    # see_features()
+    # audit_james_prentice('total_stock_value')
+    audit_wesley_colwell('from_this_person_to_poi')
