efbab707efbede929da830935331455f85fb5f18
Anna Li <annax.li10@gmail.com>
2015-06-21 18:16:03 -0700

    finished with outliers!

--- IntroMachineLearning/outliers/enron_outliers.py
+++ IntroMachineLearning/outliers/enron_outliers.py
@@ -10,6 +10,9 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+# remove TOTAL outlier
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
 outlier_salary = 0
@@ -25,12 +28,19 @@ for point in data:
         print "salary:", salary, "bonus:", bonus
         outlier_salary = salary
 
+# Find biggest outlier in data_dict
 for key in data_dict: 
-#    print key 
-#    break
+#    print key
     if data_dict[key]['salary'] == outlier_salary:
         print key, outlier_salary
 
+# find 4 outliers in cleaner dataset
+for key in data: 
+    salary = key[0]
+    if salary > 600000:
+        for p in data_dict:
+            if data_dict[p]['salary'] == salary:
+                print p
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
