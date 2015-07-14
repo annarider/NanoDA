2e021a2ee36b2a7219e9a7bebcd2cc21a723cde0
Anna Li <annax.li10@gmail.com>
2015-07-14 12:28:46 -0700

    tools text learning with stemmer

--- IntroMachineLearning/tools/parse_out_email_text.py
+++ IntroMachineLearning/tools/parse_out_email_text.py
@@ -21,19 +21,22 @@ def parseOutText(f):
     all_text = f.read()
 
     ### split off metadata
-    content = all_text.split("X-FileName:")
+    content = all_text.split("X-FileName:")    
     words = ""
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        indv_words = text_string.split()
+        stemmer = SnowballStemmer("english")
+        for word in indv_words:
+            words += stemmer.stem(word)
 
 
 
