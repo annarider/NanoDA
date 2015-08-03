8656a1ed53fa63d3c031c3b4ff7031e9d5691112
Anna Li <annax.li10@gmail.com>
2015-08-02 23:27:27 -0700

    planning feature engineering

--- IntroMachineLearning/final_project/enron_outliers_salary_payments.py
+++ IntroMachineLearning/final_project/enron_outliers_salary_payments.py
@@ -9,7 +9,7 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-features = ["poi", "salary", "shared_receipt_with_poi"]
+features = ["poi", "total_payments", "shared_receipt_with_poi"]
 
 
 # remove TOTAL outlier
@@ -20,17 +20,17 @@ outlier_salary = 0
 ### your code below
 
 for point in data:
-    color = 'y' if point[0] == 1 else 'c'
-    salary = point[1]
-    total_payments = point[2]
-    plt.scatter( salary, total_payments, color = color)
+    color = 'r' if point[0] == 1 else 'c'
+    f1 = point[1]
+    f2 = point[2]
+    plt.scatter( f1, f2, color = color)
     
     
 
     # find outlier person    
-    if salary > 800000:
-        print "salary:", salary, "total_payments:", total_payments
-        outlier_salary = salary
+    if f1 > 80000000:
+        print features[1],":", f1, features[2], ":", f1
+        outlier_salary = f1
 
 # Find outlier in data_dict
 for key in data_dict: 
@@ -40,14 +40,14 @@ for key in data_dict:
 
 # find 4 outliers in cleaner dataset
 for key in data: 
-    salary = key[1]
-    if salary > 800000:
+    f1 = key[1]
+    if f1 > 800000:
         for p in data_dict:
-            if data_dict[p]['salary'] == salary:
+            if data_dict[p][features[1]] == f1:
                 print p
 
-plt.xlabel("salary")
-plt.ylabel("total_payments")
+plt.xlabel(features[1])
+plt.ylabel(features[2])
 plt.legend()
 plt.show()
 plt.savefig("outliers.pdf")
