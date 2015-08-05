2ed639a2056f8bf8906bbf55b2d2f25c9f6a271f
Anna Li <annax.li10@gmail.com>
2015-08-05 00:26:37 -0700

    getting started with vectorizer

---
+++ IntroMachineLearning/final_project/vectorize_all_emails.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import os
+import pickle
+import re
+import sys
+
+sys.path.append( "../tools/" )
+from parse_out_email_text import parseOutText
+
+"""
+    starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification
+
+    the list of all the emails from Sara are in the from_sara list
+    likewise for emails from Chris (from_chris)
+
+    the actual documents are in the Enron email dataset, which
+    you downloaded/unpacked in Part 0 of the first mini-project
+
+    the data is stored in lists and packed away in pickle files at the end
+
+"""
+
+from_data = []
+word_data = []
+
+
+
+#%%
+
+### temp_counter helps you only look at the first 200 emails in the list
+temp_counter = 0
+
+import os
+emails_directory = os.listdir("../final_project/emails_by_address")
+for filename in emails_directory:
+    file_contents = open("../final_project/emails_by_address/" + filename, "r") 
+    
+    for email_path in file_contents:
+        temp_counter += 1
+        if temp_counter < 50:
+            path = os.path.join('../', email_path[:-1])
+#            print os.getcwd()
+            print email_path
+            email = open(path, "r")
+            print email
+#%%
+
+'''
+
+from_sara  = open("from_sara.txt", "r")
+from_chris = open("from_chris.txt", "r")
+
+
+
+
+for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+    for path in from_person:
+        ### only look at first 200 emails when developing
+        ### once everything is working, remove this line to run over full dataset
+        temp_counter += 1
+        if temp_counter < 200:
+            path = os.path.join('..', path[:-1])
+            print path
+            email = open(path, "r")
+
+            ### use parseOutText to extract the text from the opened email
+            parsed_email = parseOutText(email)
+            
+            ### use str.replace() to remove any instances of the words
+            replacements = {"sara": "", "shackleton": "", 
+                            "chris": "", "germani":"",
+                            "sshacklensf": "",
+                            "cgermannsf": ""}
+                            
+#                            
+#            replacements = dict((re.escape(k), v) 
+#                              for k, v in replacements.iteritems())
+#            pattern = re.compile("|".join(replacements.keys()))
+#            parsed_email_stripped_common_words = pattern.sub(
+#                    lambda m: replacements[re.escape(m.group(0))], parsed_email)
+                            
+            parsed_email_stripped_common_words = parsed_email
+            for word in replacements.keys():
+                parsed_email_stripped_common_words = parsed_email_stripped_common_words.replace(word, "")
+
+            ### append the text to word_data
+            word_data.append(parsed_email_stripped_common_words)
+
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'sara':
+                from_data.append(0)
+            elif name == 'chris':
+                from_data.append(1)
+
+            email.close()
+
+print "emails processed"
+from_sara.close()
+from_chris.close()
+
+pickle.dump( word_data, open("your_word_data.pkl", "w") )
+pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+
+print word_data[152]
+
+
+
+### in Part 4, do TfIdf vectorization here
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words = "english")
+vectorizer.fit_transform(word_data)
+vocab_list = vectorizer.get_feature_names()
+print "num different words:", len(vocab_list)
+print vocab_list[34597]
+'''
