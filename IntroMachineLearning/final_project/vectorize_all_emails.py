f6dfa5a3bdccd6345addd9fa1a533aa4fa6dfa36
Anna Li <annax.li10@gmail.com>
2015-08-06 21:42:42 -0700

    returned td-idf term document matrix

--- IntroMachineLearning/final_project/vectorize_all_emails.py
+++ IntroMachineLearning/final_project/vectorize_all_emails.py
@@ -27,7 +27,7 @@ def process_email_data(use_cache = False):
                 
                 for email_path in file_contents:
                     temp_counter += 1
-                    if temp_counter: # < 500:
+                    if temp_counter < 100:
                         path = os.path.join('../', email_path[:-1])
             #            print filename  
                         
@@ -57,72 +57,13 @@ def vectorize_email_data(word_data):
 #    do TfIdf vectorization here
 
     from sklearn.feature_extraction.text import TfidfVectorizer
-    vectorizer = TfidfVectorizer(stop_words = "english")
-    vectorizer.fit_transform(word_data)
-    vocab_list = vectorizer.get_feature_names()
-    return vectorizer.idf_
+    vectorizer = TfidfVectorizer(stop_words = "english", use_idf = True)
+    term_document_matrix = vectorizer.fit_transform(word_data)
+    features = vectorizer.get_feature_names()
+    return term_document_matrix, features
 
 if __name__ == '__main__':
     word_data, from_to_data = process_email_data(use_cache=False)
-    print vectorize_email_data(word_data)
+    term_document_matrix, features =  vectorize_email_data(word_data)
+    print term_document_matrix
     
-    
-
-'''
-
-from_sara  = open("from_sara.txt", "r")
-from_chris = open("from_chris.txt", "r")
-
-
-
-
-for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
-    for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
-
-            ### use parseOutText to extract the text from the opened email
-            parsed_email = parseOutText(email)
-            
-            ### use str.replace() to remove any instances of the words
-            replacements = {"sara": "", "shackleton": "", 
-                            "chris": "", "germani":"",
-                            "sshacklensf": "",
-                            "cgermannsf": ""}
-                            
-#                            
-#            replacements = dict((re.escape(k), v) 
-#                              for k, v in replacements.iteritems())
-#            pattern = re.compile("|".join(replacements.keys()))
-#            parsed_email_stripped_common_words = pattern.sub(
-#                    lambda m: replacements[re.escape(m.group(0))], parsed_email)
-                            
-            parsed_email_stripped_common_words = parsed_email
-            for word in replacements.keys():
-                parsed_email_stripped_common_words = parsed_email_stripped_common_words.replace(word, "")
-
-            ### append the text to word_data
-            word_data.append(parsed_email_stripped_common_words)
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            if name == 'sara':
-                from_data.append(0)
-            elif name == 'chris':
-                from_data.append(1)
-
-            email.close()
-
-print "emails processed"
-from_sara.close()
-from_chris.close()
-
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-print word_data[152]
-'''
