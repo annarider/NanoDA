da5e25664d507ee237f5d3d8d787b0ac652f95f7
Anna Li <annax.li10@gmail.com>
2015-08-16 03:37:46 -0700

    update analysis for topic cluster total & poi counts using dicts

--- IntroMachineLearning/final_project/k-means-cluster-analysis.py
+++ IntroMachineLearning/final_project/k-means-cluster-analysis.py
@@ -1,10 +1,11 @@
 import operator
 import pickle
-import sklearn
 import numpy as np
+#from sklearn.cluster import KMeans
 
 KMEANS_FILE_NAME_PATH = "k-means_60clusters_subset.pkl"
-FROM_TO_FILE_NAME_PATH = "from_to_kmeans_60clusters_subset.pkl"
+#FROM_TO_FILE_NAME_PATH = "from_to_kmeans_60clusters_subset.pkl"
+WORD_DATA_FILE_PATH = "word_data.pkl"
 FINAL_PROJECT_DATASET = "final_project_dataset.pkl"
 TFIDF_MATRIX_PATH = "tfidf_matrix.pkl"
 CLUSTER_RATIO_SUBSET_PERCENT = 0.2
@@ -13,16 +14,18 @@ CLUSTER_RATIO_SUBSET_PERCENT = 0.2
 #from_to_data = from_to_data_subset
 
 with open(KMEANS_FILE_NAME_PATH, 'r') as km:
-    pred, cluster_centers, labels = pickle.load(km)
-with open(FROM_TO_FILE_NAME_PATH, 'r') as ft:
-    from_to_data = pickle.load(ft)
+    pred, clf, cluster_centers, labels = pickle.load(km)
+#with open(FROM_TO_FILE_NAME_PATH, 'r') as ft:
+#    from_to_data = pickle.load(ft)
 with open(FINAL_PROJECT_DATASET, "r") as fpd:
     final_project_dataset = pickle.load(fpd)
 with open(TFIDF_MATRIX_PATH, "r") as tm:
     term_document_matrix = pickle.load(tm)
+with open(WORD_DATA_FILE_PATH, "r") as wd:
+    word_data, from_to_data = pickle.load(wd)
     
 #forgot to pickle clf, so reconstruct it.
-clf = sklearn.cluster.k_means(cluster_centers,cluster_centers.shape[0],init=cluster_centers)
+#clf = KMeans(cluster_centers,n_clusters=cluster_centers.shape[0],init=cluster_centers)
 
 #use clf.predict(fullTfIdfMatrix) to get the cluster label of each email
 
@@ -32,24 +35,25 @@ email_topic_clusters = clf.predict(term_document_matrix)
 clusters_poi_count = dict.fromkeys(range(0, 60))
 clusters_total_email_count = dict.fromkeys(range(0, 60))
 
-# count how many emails in each cluster
-for cluster_num in email_topic_clusters:
-    if clusters_total_email_count[cluster_num] is None:
-        clusters_total_email_count[cluster_num] = 1
-    else: 
-        clusters_total_email_count[cluster_num] = clusters_total_email_count[cluster_num] + 1
-
+# dict for checking if POI using email address 
 check_email_poi = dict([(final_project_dataset[person]['email_address'], final_project_dataset[person]['poi']) for person in final_project_dataset])
 
-
-# count how many emails from pois in each cluster
+# counting total emails & num of POIs
 for i, cluster_num in enumerate(email_topic_clusters):
     email, _ = from_to_data[i]
-
-                if clusters_poi_count[cluster_num] is not None:
-                    clusters_poi_count[cluster_num] += 1
-                else: 
-                    clusters_poi_count[cluster_num] = 1
+    # count how many emails in each topic cluster
+    if clusters_total_email_count[cluster_num] is not None:
+        clusters_total_email_count[cluster_num] += 1
+    else:
+        clusters_total_email_count[cluster_num] = 1
+    # count how many emails from pois in each topic cluster
+    if email in check_email_poi and check_email_poi[email] == True:
+        if clusters_poi_count[cluster_num] is not None:
+            clusters_poi_count[cluster_num] += 1
+        else: 
+            clusters_poi_count[cluster_num] = 1 
+    
+                
             
                     
 clusters_poi_ratio = {}
