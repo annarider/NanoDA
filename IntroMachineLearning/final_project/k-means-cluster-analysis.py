282bd0ccb68b4db415a02c385eb406289f0d30fe
Anna Li <annax.li10@gmail.com>
2015-08-16 17:10:43 -0700

    calculating ratio of suspicious emails versus total emails

--- IntroMachineLearning/final_project/k-means-cluster-analysis.py
+++ IntroMachineLearning/final_project/k-means-cluster-analysis.py
@@ -8,6 +8,7 @@ KMEANS_FILE_NAME_PATH = "k-means_60clusters_subset.pkl"
 WORD_DATA_FILE_PATH = "word_data.pkl"
 FINAL_PROJECT_DATASET = "final_project_dataset.pkl"
 TFIDF_MATRIX_PATH = "tfidf_matrix.pkl"
+CACHE_EMAIL_TOPIC_CLUSTERS = "email_topic_clusters.pkl"
 CLUSTER_RATIO_SUBSET_PERCENT = 0.2
 N_CLUSTERS = 60
 TOP_N_CLUSTERS = 5
@@ -32,12 +33,13 @@ clf.fit(cluster_centers)
 
 #get the cluster label of each email
 email_topic_clusters = clf.predict(term_document_matrix)
+with open(CACHE_EMAIL_TOPIC_CLUSTERS, "w") as etc:
+    pickle.dump(email_topic_clusters, etc, protocol = pickle.HIGHEST_PROTOCOL)
+
 
 ## calculate email & POI occurences
 clusters_poi_count = np.zeros(N_CLUSTERS)
 clusters_total_email_count = np.zeros(N_CLUSTERS)
-#clusters_poi_count = dict.fromkeys(range(0, N_CLUSTERS))
-#clusters_total_email_count = dict.fromkeys(range(0, N_CLUSTERS))
 
 # dict for checking if POI using email address 
 check_email_poi = dict([(final_project_dataset[person]['email_address'], final_project_dataset[person]['poi']) for person in final_project_dataset])
@@ -57,9 +59,6 @@ clusters_poi_ratio = np.zeros(N_CLUSTERS)
 for i, c in enumerate(clusters_poi_count):
     clusters_poi_ratio[i] = clusters_poi_count[i] / clusters_total_email_count[i]
 
-#clusters_poi_ratio_sorted = sorted(clusters_poi_ratio.items(), key=operator.itemgetter(1), reverse = True)               
-#clusters_poi_ratio_top_sorted = clusters_poi_ratio_sorted[:int(len(clusters_poi_ratio_sorted) * CLUSTER_RATIO_SUBSET_PERCENT)]
-
 # sort by clusters ratio of POI/total email ascending
 clusters_poi_ratio_sorted = np.argsort(clusters_poi_ratio)
 # Find the top corresponding clusters, i.e. "suspicious clusters"
@@ -70,3 +69,20 @@ clusters_poi_ratio_top_sorted = clusters_poi_ratio_sorted[-TOP_N_CLUSTERS:]
 # identify the 5 clusters that have the most POI emails. Call these "suspicious clusters"
 # for each user, count the number of emails in suspicious clusters
 # divide that by the total emails from that users. The resulting float is the new feature 
+
+email_suspicious_cluster_count = dict([(final_project_dataset[person]['email_address'], 0) for person in final_project_dataset])
+email_total_count = dict([(final_project_dataset[person]['email_address'], 0) for person in final_project_dataset])
+
+for i, cluster_num in enumerate(email_topic_clusters):
+    email, _ = from_to_data[i]
+    if email in email_total_count:
+        email_total_count[email] += 1
+        if cluster_num in clusters_poi_ratio_top_sorted: 
+            email_suspicious_cluster_count[email] += 1
+
+email_suspicious_total_ratio = {}
+for email in email_total_count:
+    if email_total_count[email] != 0:
+        email_suspicious_total_ratio[email] = email_suspicious_cluster_count[email] / email_total_count[email]
+    else:
+        email_suspicious_total_ratio[email] = 0        
