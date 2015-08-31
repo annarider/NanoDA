43e53c2084b1ca9d6bc322178e2d5c2de2021365
Anna Li <annax.li10@gmail.com>
2015-08-30 20:46:24 -0700

    prepare for write up

--- IntroMachineLearning/final_project/poi_id.py
+++ IntroMachineLearning/final_project/poi_id.py
@@ -73,19 +73,18 @@ from sklearn import grid_search
 from sklearn.metrics import make_scorer, recall_score
 recall_scorer = make_scorer(recall_score, greater_is_better=True) 
 
-parameters = {'min_samples_split': [2, 5, 10],
+parameters = {'min_samples_split': [2, 3, 4],
                       'min_samples_leaf': [1, 2],
                       'max_depth': [None, 1],
                       'splitter' : ['best', 'random'],
                       'criterion': ['gini', 'entropy'],
-                      'random_state': [42]
 }
 #
 clf_grid_search = grid_search.GridSearchCV(clf, parameters, scoring=recall_scorer)
-
-#clf = tree.DecisionTreeClassifier(compute_importances=None, criterion='entropy',
-#            max_depth=None, max_features=10, min_density=None,
-#            min_samples_leaf=1, min_samples_split=1, random_state=None,
+#
+#clf = tree.DecisionTreeClassifier(criterion='gini',
+#            max_depth=None, max_features=None, min_density=None,
+#            min_samples_leaf=1, min_samples_split=2, random_state=None,
 #            splitter='best')
 #test_classifier(clf, my_dataset, features_list_best)
 test_classifier(clf_grid_search, my_dataset, features_list_best)
